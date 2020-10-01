985725c16982f105e440bbd41fb6f7fc5c3be750
sacha smets <sacha.smets@student.ehb.be>
2020-10-01 14:43:04 +0200

    solved decryptor minus key instantiation (test wrong?)

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -1,5 +1,9 @@
-module.exports = () => {
+const nacl = require('libsodium-wrappers')
+
+module.exports = (key) => {
     return Object.freeze({
-        decrypt: () => {}
+        decrypt: (ciphertext, nonce) => {
+            return nacl.crypto_secretbox_open_easy(ciphertext,nonce,key);
+        }
     })
 }
